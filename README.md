12117fcbb3b01f12737a1ff24e7f07cdd3ff478d
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2024-09-25 23:19:14 +0900

    day2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # study-rust-websockets-tokio-test
 
 https://www.youtube.com/watch?v=p1ES0apOwUw
-上記を参考に実装中　現在12:20
+上記を参考に実装中　現在17:10
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 #[tokio::main]
 async  fn main() {
-    let url = "wss://echo.websocket.events";
+    // let url = "wss://echo.websocket.events";
+    let url = "ws://localhost:3000";
 
     println!("Connecting to - {}",url);
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connecting");
@@ -11,13 +12,12 @@ async  fn main() {
 
     let (mut write, mut read) = ws_stream.split();
 
-    let msg = Message::Text("aloha echo server".into());
+    let bot_name = "RustTimeBot";
+    let msg = Message::Text(format!("register as {}", bot_name).into());
+    println!("sending message: {}", msg);
+    write.send(msg).await.expect("Failed to send message");
 
-    if let Some(message) = read.next().await{
-        let message = message.expect("Failed to read the message");
-        println!("Received a message: {}", message);
-    }
-
+    let msg = Message::Text("who's connected?".into());
     println!("sending message: {}", msg);
     write.send(msg).await.expect("Failed to send message");
 
